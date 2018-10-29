9cff43527b3717b96b8217775cf78942f792d89c
CPayne6 <cpaynetrayne6@gmail.com>
2018-10-29 17:08:32 -0400

    JavaScript Tutorial 2 added

---
+++ Javascript Implementation/Tutorial-Finished/studentChart.js
@@ -0,0 +1,61 @@
+var context = document.getElementById("Chart1").getContext("2d");
+
+
+
+var myChart = new Chart(context, {
+
+	// Declare the type of Bar
+	type: 'bar',
+
+	// Assign the Data
+	data:{
+		labels: ['Red', 'Blue', "Yellow", 'Green'],
+		datasets:[{
+			label: '# of Votes',
+			data: [12,19,3,5],
+
+			backgroundColor: ['green','yellow','blue', 'red' ]
+
+		}]
+	},
+
+	// Chart Options 
+
+	options:{
+		
+	}
+
+
+});
+
+
+var context2 =document.getElementById("Chart2").getContext("2d");
+
+var Chart2 = new Chart(context2,{
+
+
+	type: 'pie',
+
+	data:{
+
+		labels: ["Africa" , 'Europe', "Asia", "North America"],
+		datasets:[{
+
+			label: "Population (Millions)",
+			backgroundColor: ['red', 'green', 'purple', 'yellow'],
+
+			data: [2478,5367,735,800]
+		}]
+	},
+
+	options:{
+
+		title: {
+			display: true,
+
+			text: 'Predicted World Population in 2040'
+
+		}
+		
+	}
+});
